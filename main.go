4f4770e1ffe2c5a378ce7e422cc4d583ef6d5d15
enzosv <vergara.enzo@gmail.com>
2024-04-10 13:30:50 +0800

    graceful shutdown

--- main.go
+++ main.go
@@ -5,26 +5,45 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
+
 	db, err := sql.Open("sqlite", "./medium.db")
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
+	defer db.Close()
 	db.Exec("PRAGMA journal_mode=WAL;")
 	db.Exec("PRAGMA locking_mode=IMMEDIATE;")
-	db.Exec("pragma synchronous = normal;	")
-	db.Exec("pragma temp_store = memory;	")
-	defer db.Close()
+	db.Exec("pragma synchronous = normal;")
+	db.Exec("pragma temp_store = memory;")
 	// migrate(db)
 	ctx := context.Background()
 	queueChan := make(chan string)
 	go func() {
 		err = queryPages(ctx, db, queueChan)
 		if err != nil {
-			log.Fatal("queue error", err)
+			log.Panic("queue error", err)
 		}
+	}()
+	startCount := 0
+	endCount := 0
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT)
+	go func() {
+		<-sigc
+		fmt.Printf("found %d new posts overall\n", endCount-startCount)
+		db.Exec("vaccum;")
+		db.Close()
+		os.Exit(0)
 	}()
 
 	for {
@@ -33,7 +52,7 @@ func main() {
 		for {
 			parsed, newNext, err := importMedium(q, next)
 			if err != nil {
-				log.Fatal("fetch error", err)
+				log.Panic("fetch error", err)
 			}
 			if len(parsed.collections) == 0 && len(parsed.tags) == 0 &&
 				len(parsed.users) == 0 && len(parsed.posts) == 0 {
@@ -42,23 +61,26 @@ func main() {
 			// go func() {
 			previousCount, err := countPosts(ctx, db)
 			if err != nil {
-				log.Fatal("count error", err)
+				log.Panic("count error", err)
+			}
+			if startCount == 0 {
+				startCount = previousCount
 			}
 			fmt.Printf("saving\n\t%d posts\n\t%d users\n\t%d collections\n\t%d tags\n",
 				len(parsed.posts), len(parsed.users), len(parsed.collections), len(parsed.tags))
 			err = save(ctx, db, parsed)
 			if err != nil {
-				log.Fatal("save error", err)
+				log.Panic("save error", err)
 			}
 			err = logPage(ctx, db, q)
 			if err != nil {
-				log.Fatal("log error", err)
+				log.Panic("log error", err)
 			}
-			newCount, err := countPosts(ctx, db)
+			endCount, err = countPosts(ctx, db)
 			if err != nil {
-				log.Fatal("count 2 error", err)
+				log.Panic("count 2 error", err)
 			}
-			fmt.Printf("found %d new posts\n", newCount-previousCount)
+			fmt.Printf("found %d new posts\n", endCount-previousCount)
 			// if newCount == previousCount {
 			// fast mode
 			// break
