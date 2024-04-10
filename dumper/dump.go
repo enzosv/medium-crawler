bcef50e3f9a3536cfb0b6e521df492c012363c27
enzosv <vergara.enzo@gmail.com>
2024-04-10 12:51:33 +0800

    use modernc for sqlite in dump

--- dumper/dump.go
+++ dumper/dump.go
@@ -8,7 +8,7 @@ import (
 	"os"
 	"strings"
 
-	_ "github.com/mattn/go-sqlite3"
+	_ "modernc.org/sqlite"
 )
 
 type Post struct {
@@ -26,11 +26,11 @@ type Post struct {
 func main() {
 	posts, err := query()
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
 	err = toCSV(posts)
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
 }
 
