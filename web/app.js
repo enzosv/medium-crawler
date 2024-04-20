9587ca5d8ada9f7ed48ba04a37fd78ad153de686
enzosv <vergara.enzo@gmail.com>
2024-04-20 09:04:44 +0800

    update web

--- web/app.js
+++ web/app.js
@@ -1,33 +1,19 @@
-const is_omnivore = window.location.href.includes("omnivore");
-
-function buildLink(url) {
-  if (is_omnivore) {
-    return `<a href="javascript:void(0);" onclick="omnivore('${url}')">`;
-    // return `<a href=https://omnivore.app/api/save?url=${url}`;
-  }
-  return `<a href=${url}>`;
-}
-
-async function main() {
-  const res = await fetch("./medium.json");
-  const data = await res.json();
-  const freedium = window.location.href.includes("freedium");
-  const prefix =
-    freedium || is_omnivore
-      ? "https://freedium.cfd/https://medium.com/articles/"
-      : "https://medium.com/articles/";
+function setupTable(rows, prefix) {
   const table = $("#example").DataTable({
-    data: data,
+    data: rows,
     ordering: false,
     order: [[1, "desc"]],
     columns: [
       {
         data: "title",
         render: function (data, type, row) {
+          const link = row.is_paid
+            ? prefix + row.post_id
+            : "https://medium.com/articles/" + row.post_id;
           return `<div class="row">
-            ${buildLink(prefix + row[2])}
-            <h5>${row[0]}</a> ${
-            row[9] == 0
+          <a href=${link}>
+            <h5>${row.title}</a> ${
+            row.is_paid == 0
               ? ""
               : `<img src="paywall-svgrepo-com.svg" width="16" height="16"/>`
           }</h5>
@@ -35,47 +21,61 @@ async function main() {
           <div class="row">
             <div class="col-auto">
               <img src="calendar-arrow-up-svgrepo-com.svg" width="16" height="16"/><small> ${formatDate(
-                row[3]
+                row.published_at
               )}</small>
             </div>
             <div class="col-auto">
               <small>${
-                row[4]
-                  ? `<img src="collection-svgrepo-com.svg" width="16" height="16"/> ${row[4]}`
+                row.collection
+                  ? `<img src="collection-svgrepo-com.svg" width="16" height="16"/> ${row.collection}`
                   : ""
               }</small>
             </div>
             <div class="col-auto">
-            <small>${row[10] ? `by ${row[10]}` : ""}</small>
+            <small>${row.author ? `by ${row.author}` : ""}</small>
             </div>
           </div>
           <div class="row">
             <div class="col-auto">
               <img src="clap-svgrepo-com.svg" width="16" height="16"/> ${cleanNumber(
-                row[1]
+                row.total_clap_count
               )}
             </div>
             <div class="col-auto">
               <img src="time-svgrepo-com.svg" width="16" height="16"/> ${cleanNumber(
-                row[7]
+                row.reading_time
               )}
             </div>
             <div class="col-auto">
               <img src="share-svgrepo-com.svg" width="16" height="16"/> ${cleanNumber(
-                row[5]
+                row.recommend_count
               )}
             </div>
           <div class="col-auto">
             <img src="comment-svgrepo-com.svg" width="16" height="16"/> ${cleanNumber(
-              row[6]
+              row.response_count
             )}
           </div>
           <div class="row">
-          <small>${row[8] ? row[8].split(",").join(", ") : ""}</small>
+          <small>${row.tags ? row.tags.split(",").join(", ") : ""}</small>
           </div>
-          <button type="button" class="btn btn-link">
+          <div class="row">
+          <div class="col-auto">
+          <button id="close" type="button" class="btn btn-link">
+          <img src="close-svgrepo-com.svg" width="24" height="24"/>
+          </button>
+          </div>
+          <div class="col-auto">
+          <button id="share" type="button" class="btn btn-link">
           <img src="share-ios-export-svgrepo-com.svg" width="24" height="24"/>
           </button>
+          </div>
+          <div class="col-auto">
+          <button id="check" type="button" class="btn btn-link">
+          <img src="check-svgrepo-com.svg" width="24" height="24"/>
+          </button>
+          </div>
+          </div>
         </div>`;
         },
       },
@@ -83,14 +83,107 @@ async function main() {
   });
 
   table.on("click", "button", function (e) {
-    const data = table.row(e.target.closest("tr")).data();
-    share(data[0], prefix + data[2]);
+    const row = table.row(e.target.closest("tr"));
+    const data = row.data();
+    const link = data.is_paid
+      ? prefix + data.post_id
+      : "https://medium.com/articles/" + data.post_id;
+    handleButton(e.currentTarget.id, data, link);
+    row.remove().draw();
   });
 
   table.on("touchend", "button", function (e) {
-    const data = table.row(e.target.closest("tr")).data();
-    share(data[0], prefix + data[2]);
+    const row = table.row(e.target.closest("tr"));
+    const data = row.data();
+    const link = data.is_paid
+      ? prefix + data.post_id
+      : "https://medium.com/articles/" + data.post_id;
+    handleButton(e.currentTarget.id, data, link);
+    row.remove().draw();
   });
+  return table;
+}
+async function main() {
+  const res = await fetch("./medium.json");
+  let data = await res.json();
+  const disliked = [];
+  const liked = [];
+  const def = [];
+  const shared = [];
+  const alldata = [];
+  for (const d of data) {
+    const row = {
+      title: d[0],
+      claps: d[1],
+      post_id: d[2],
+      published_at: d[3],
+      collection: d[4],
+      recommend_count: d[5],
+      response_count: d[6],
+      reading_time: d[7],
+      tags: d[8],
+      is_paid: d[9],
+      author: d[10],
+    };
+    const toggle = localStorage.getItem(row.post_id);
+    if (toggle == 1) {
+      liked.push(row);
+    } else if (toggle == -1) {
+      disliked.push(row);
+    } else if (toggle == 0) {
+      shared.push(row);
+    } else {
+      def.push(row);
+    }
+    alldata.push(row);
+  }
+  data = undefined;
+  const freedium = window.location.href.includes("freedium");
+  const prefix = freedium
+    ? "https://freedium.cfd/"
+    : "https://medium.com/articles/";
+  const table = setupTable(def, prefix);
+
+  $("#default").on("click", function () {
+    table.clear();
+    table.rows.add(def);
+    table.draw();
+  });
+  $("#liked").on("click", function () {
+    table.clear();
+    table.rows.add(liked);
+    table.draw();
+  });
+  $("#disliked").on("click", function () {
+    table.clear();
+    table.rows.add(disliked);
+    table.draw();
+  });
+  $("#shared").on("click", function () {
+    table.clear();
+    table.rows.add(shared);
+    table.draw();
+  });
+  $("#all").on("click", function () {
+    table.clear();
+    table.rows.add(alldata);
+    table.draw();
+  });
+}
+
+function handleButton(id, data, link) {
+  switch (id) {
+    case "close":
+      localStorage.setItem(data.post_id, -1);
+      return;
+    case "share":
+      share(data.title, link);
+      localStorage.setItem(data.post_id, 0);
+      return;
+    case "check":
+      localStorage.setItem(data.post_id, 1);
+      return;
+  }
 }
 
 function formatDate(date) {
@@ -127,18 +220,6 @@ function share(title, link) {
   console.log(navigator);
 }
 
-// ${
-//   freedium
-//     ? `
-//     <div class="row">
-//       <a title="Save to Omnivore" onclick="omnivore('${prefix}${row[2]}')" href="javascript:void(0);">
-//       <svg width="26" height="26" fill="none">
-//       <path d="M8.42285 17.9061V10.5447C8.42285 9.91527 9.16173 9.55951 9.65432 9.99737L11.9257 13.3087C12.3909 13.6918 13.0477 13.6918 13.5129 13.3087L15.7296 10.0247C16.2222 9.61424 16.961 9.94263 16.961 10.5721V14.458C16.961 16.3463 18.2199 17.8788 20.1081 17.8788H20.1629C21.7775 17.8788 23.1731 16.7841 23.5563 15.2243C23.7478 14.4033 23.912 13.5549 23.912 12.8982C23.8847 6.46715 18.4388 1.596 11.9257 2.03385C6.39776 2.41698 1.9371 6.87764 1.55397 12.4056C1.11612 18.9187 6.26093 24.3645 12.7193 24.3645" stroke="white" stroke-width="2.18182" stroke-miterlimit="10"></path>
-//       </svg></a>
-//     </div>`
-//     : ""
-// }
-
 function cleanNumber(number) {
   if (number > 1000) {
     return Math.round(number / 1000) + "k";
@@ -146,50 +227,6 @@ function cleanNumber(number) {
   return Math.round(number);
 }
 
-async function omnivore(link) {
-  key = localStorage.getItem("omnivore");
-  if (!key) {
-    key = prompt("omnivore api key");
-    localStorage.setItem("omnivore", key);
-  }
-  const body = {
-    query:
-      "mutation SaveUrl($input: SaveUrlInput!) { saveUrl(input: $input) { ... on SaveSuccess { url clientRequestId } ... on SaveError { errorCodes message } } }",
-    variables: {
-      input: {
-        source: "api",
-        url: link,
-        clientRequestId: crypto.randomUUID(),
-        labels: [{ name: "medium-crawler" }],
-      },
-    },
-  };
-
-  const response = await fetch("https://api-prod.omnivore.app/api/graphql", {
-    body: JSON.stringify(body),
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: localStorage.getItem("omnivore"),
-    },
-    method: "POST",
-  });
-  const data = await response.json();
-  const url = data?.data?.saveUrl?.url;
-
-  if (omnivore && url) {
-    window.open(url);
-    return;
-  }
-  const toastLiveExample = document.getElementById("liveToast");
-  const toast = new bootstrap.Toast(toastLiveExample);
-  const toastMessage = document.getElementById("toast");
-  if (url) {
-    toastMessage.innerHTML = `Saved! <a href=${url}>${url}</a>`;
-  } else {
-    console.log(response);
-    toastMessage.innerHTML = JSON.stringify(data);
-  }
-
-  toast.show();
-}
-main();
+$("document").ready(function () {
+  main();
+});
